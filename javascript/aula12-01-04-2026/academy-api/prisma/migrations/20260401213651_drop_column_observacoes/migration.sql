5f4794b92477a70d93068121135020225e3474d0
Leticia Leal <leticia.leal@growdev.academy>
2026-04-01 18:54:56 -0300

    setup aula

---
+++ javascript/aula12-01-04-2026/academy-api/prisma/migrations/20260401213651_drop_column_observacoes/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `observacoes` on the `pessoas` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "academy_api"."pessoas" DROP COLUMN "observacoes";
